5e2ad2e38abd95483d5ca8d389e717a0bd3c1f38
strdoner <borisdudec@gmail.com>
2025-04-20 13:41:28 +0800

    init frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import WelcomePage from './pages/WelcomePage';
+import './styles/style.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min";
+
+function App() {
+  return (
+    <WelcomePage />
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/WelcomePage.jsx
@@ -0,0 +1,14 @@
+import {observer} from 'mobx-react-lite'
+
+
+function WelcomePage() {
+    return (
+        <div className='container'>
+            <div className='text-center'>
+                <h2>Hello world</h2>
+            </div>
+        </div>
+    )
+}
+
+export default WelcomePage;
